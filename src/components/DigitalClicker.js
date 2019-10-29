c277b4aa338e583be94a6f3df251fcf4888a1a3d
github username <github email address>
2019-10-29 09:36:25 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        let increment = this.state.timesClicked + 1
+        return(
+            this.setState({
+                timesClicked: increment
+            })
+        )
+    }
+
+    render(){
+        return(
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        )
+    }
+
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,56 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    bitrateClick = () => {
+        this.setState({settings: {...this.state.settings, bitrate: 12}})
+
+        // console.log(this.state.settings.bitrate)
+    }
+
+    resolutionClick = () => {
+        this.setState({settings: {...this.state.settings, video: {...this.state.settings.video, resolution: '720p'}}})
+        
+        // console.log(this.state.settings.video.resolution)
+    }
+
+    // bitrateClick = () => {
+    //     let updateBitrate = {...this.state.settings}
+    //     updateBitrate.bitrate = 12
+    //     this.setState({updateBitrate})
+
+    //     console.log(this.state.settings.bitrate)
+    // }
+
+    // resolutionClick = () => {
+    //     let updateResolution = {...this.state.settings.video}
+    //     updateResolution.resolution = '720p'
+    //     this.setState({updateResolution})
+
+    //     console.log(this.state.settings.video.resolution)
+    // }
+
+    render(){
+        return(
+            <div>
+                <button className='bitrate' onClick={this.bitrateClick}>Bitrate</button>
+                <button className='resolution' onClick={this.resolutionClick}>Resolution</button>
+            </div>
+        )
+    }
+
+}
